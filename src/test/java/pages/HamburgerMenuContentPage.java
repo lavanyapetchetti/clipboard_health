29ecc2768ba5852344bfa28036d9e2b0fb74b0c7
lavanyapetchetti <99505509+lavanyapetchetti@users.noreply.github.com>
2022-06-18 21:25:26 -0700

    last changes to pages files

--- src/test/java/pages/HamburgerMenuContentPage.java
+++ src/test/java/pages/HamburgerMenuContentPage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.NoSuchElementException;
 
@@ -23,7 +24,6 @@ public class HamburgerMenuContentPage {
 
     @FindBy(how = How.XPATH, using= "//*[contains(@class, 'hmenu-item') and text() = 'Televisions']")
     private WebElement hamburger_menu_item_tv_sub;
-
 
     public void clickOnTvFromMenuItem() {
         ExpectedConditions.visibilityOf(hamburger_menu_item_tv);
