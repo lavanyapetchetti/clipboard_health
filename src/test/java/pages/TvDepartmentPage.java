176c91bd445a7ffcf1b7995835587f85ce7c8ef2
lavanyapetchetti <99505509+lavanyapetchetti@users.noreply.github.com>
2022-06-18 21:29:21 -0700

    Update TvDepartmentPage.java

--- src/test/java/pages/TvDepartmentPage.java
+++ src/test/java/pages/TvDepartmentPage.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
